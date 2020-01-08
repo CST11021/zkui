0e988fb7659f8c57335db9a8db84666db7d09446
wanghz <wanghz@tuya.com>
2020-01-08 21:02:42 +0800

    笔记

--- src/main/java/com/deem/zkui/controller/Home.java
+++ src/main/java/com/deem/zkui/controller/Home.java
@@ -110,86 +110,44 @@ public class Home extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         logger.debug("Home Post Action!");
         try {
+            // 获取全局配置
             Properties globalProps = (Properties) this.getServletContext().getAttribute("globalProps");
             Dao dao = new Dao(globalProps);
+
+            // 获取zk服务器
             String zkServer = globalProps.getProperty("zkServer");
             String[] zkServerLst = zkServer.split(",");
 
+
+            // 获取请求参数
             Map<String, Object> templateParam = new HashMap<>();
-            String action = request.getParameter("action");
             String currentPath = request.getParameter("currentPath");
             String displayPath = request.getParameter("displayPath");
             String newProperty = request.getParameter("newProperty");
             String newValue = request.getParameter("newValue");
             String newNode = request.getParameter("newNode");
-
             String[] nodeChkGroup = request.getParameterValues("nodeChkGroup");
             String[] propChkGroup = request.getParameterValues("propChkGroup");
-
             String searchStr = request.getParameter("searchStr").trim();
-            String authRole = (String) request.getSession().getAttribute("authRole");
 
+
+            String action = request.getParameter("action");
+            String authRole = (String) request.getSession().getAttribute("authRole");
             switch (action) {
                 case "Save Node":
-                    if (!newNode.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-                        //Save the new node.
-                        ZooKeeperUtil.INSTANCE.createFolder(currentPath + newNode, "foo", "bar", ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-                        request.getSession().setAttribute("flashMsg", "Node created!");
-                        dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Creating node: " + currentPath + newNode);
-                    }
-                    response.sendRedirect("/home?zkPath=" + displayPath);
+                    saveNode(request, response, globalProps, dao, zkServerLst, newNode, currentPath, authRole, displayPath);
                     break;
                 case "Save Property":
-                    if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-                        //Save the new node.
-                        ZooKeeperUtil.INSTANCE.createNode(currentPath, newProperty, newValue, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-                        request.getSession().setAttribute("flashMsg", "Property Saved!");
-                        if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
-                            newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
-                        }
-                        dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Saving Property: " + currentPath + "," + newProperty + "=" + newValue);
-                    }
-                    response.sendRedirect("/home?zkPath=" + displayPath);
+                    saveProperty(request, response, globalProps, dao, zkServerLst, newProperty, newValue, currentPath, authRole, displayPath);
                     break;
                 case "Update Property":
-                    if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-                        //Save the new node.
-                        ZooKeeperUtil.INSTANCE.setPropertyValue(currentPath, newProperty, newValue, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-                        request.getSession().setAttribute("flashMsg", "Property Updated!");
-                        if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
-                            newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
-                        }
-                        dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Updating Property: " + currentPath + "," + newProperty + "=" + newValue);
-                    }
-                    response.sendRedirect("/home?zkPath=" + displayPath);
+                    updateProperty(request, response, globalProps, dao, zkServerLst, newProperty, newValue, currentPath, authRole, displayPath);
                     break;
                 case "Search":
-                    Set<LeafBean> searchResult = ZooKeeperUtil.INSTANCE.searchTree(searchStr, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps), authRole);
-                    templateParam.put("searchResult", searchResult);
-                    ServletUtil.INSTANCE.renderHtml(request, response, templateParam, "search.ftl.html");
+                    search(request, response, globalProps, dao, zkServerLst, templateParam, searchStr, authRole);
                     break;
                 case "Delete":
-                    if (authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-
-                        if (propChkGroup != null) {
-                            for (String prop : propChkGroup) {
-                                List delPropLst = Arrays.asList(prop);
-                                ZooKeeperUtil.INSTANCE.deleteLeaves(delPropLst, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-                                request.getSession().setAttribute("flashMsg", "Delete Completed!");
-                                dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Deleting Property: " + delPropLst.toString());
-                            }
-                        }
-                        if (nodeChkGroup != null) {
-                            for (String node : nodeChkGroup) {
-                                List delNodeLst = Arrays.asList(node);
-                                ZooKeeperUtil.INSTANCE.deleteFolders(delNodeLst, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-                                request.getSession().setAttribute("flashMsg", "Delete Completed!");
-                                dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Deleting Nodes: " + delNodeLst.toString());
-                            }
-                        }
-
-                    }
-                    response.sendRedirect("/home?zkPath=" + displayPath);
+                    delete(request, response, globalProps, dao, zkServerLst, nodeChkGroup, propChkGroup, authRole, displayPath);
                     break;
                 default:
                     response.sendRedirect("/home");
@@ -199,5 +157,169 @@ public class Home extends HttpServlet {
             logger.error(Arrays.toString(ex.getStackTrace()));
             ServletUtil.INSTANCE.renderError(request, response, ex.getMessage());
         }
+    }
+
+    /**
+     * 保存节点
+     *
+     * @param request
+     * @param response
+     * @param globalProps
+     * @param dao
+     * @param zkServerLst
+     * @param newNode
+     * @param currentPath
+     * @param authRole
+     * @param displayPath
+     * @throws KeeperException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void saveNode(HttpServletRequest request, HttpServletResponse response, Properties globalProps, Dao dao, String[] zkServerLst, String newNode, String currentPath, String authRole, String displayPath) throws KeeperException, InterruptedException, IOException {
+        if (!newNode.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
+            ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+
+            ZooKeeperUtil.INSTANCE.createFolder(currentPath + newNode, "foo", "bar", zookeeper);
+            request.getSession().setAttribute("flashMsg", "Node created!");
+
+            String user = (String) request.getSession().getAttribute("authName");
+            dao.insertHistory(user, request.getRemoteAddr(), "Creating node: " + currentPath + newNode);
+        }
+        response.sendRedirect("/home?zkPath=" + displayPath);
+    }
+
+    /**
+     * 保存属性
+     *
+     * @param request
+     * @param response
+     * @param globalProps
+     * @param dao
+     * @param zkServerLst
+     * @param newProperty
+     * @param newValue
+     * @param currentPath
+     * @param authRole
+     * @param displayPath
+     * @throws KeeperException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void saveProperty(HttpServletRequest request, HttpServletResponse response, Properties globalProps, Dao dao, String[] zkServerLst, String newProperty, String newValue, String currentPath, String authRole, String displayPath) throws KeeperException, InterruptedException, IOException {
+        if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
+
+            ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+
+            ZooKeeperUtil.INSTANCE.createNode(currentPath, newProperty, newValue, zookeeper);
+            request.getSession().setAttribute("flashMsg", "Property Saved!");
+            if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
+                newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
+            }
+
+            String user = (String) request.getSession().getAttribute("authName");
+            dao.insertHistory(user, request.getRemoteAddr(), "Saving Property: " + currentPath + "," + newProperty + "=" + newValue);
+        }
+        response.sendRedirect("/home?zkPath=" + displayPath);
+    }
+
+    /**
+     * 更新属性
+     *
+     * @param request
+     * @param response
+     * @param globalProps
+     * @param dao
+     * @param zkServerLst
+     * @param newProperty
+     * @param newValue
+     * @param currentPath
+     * @param authRole
+     * @param displayPath
+     * @throws KeeperException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void updateProperty(HttpServletRequest request, HttpServletResponse response, Properties globalProps, Dao dao, String[] zkServerLst, String newProperty, String newValue, String currentPath, String authRole, String displayPath) throws KeeperException, InterruptedException, IOException {
+        if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
+            ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+            ZooKeeperUtil.INSTANCE.setPropertyValue(currentPath, newProperty, newValue, zookeeper);
+            request.getSession().setAttribute("flashMsg", "Property Updated!");
+            if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
+                newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
+            }
+
+            String user = (String) request.getSession().getAttribute("authName");
+            dao.insertHistory(user, request.getRemoteAddr(), "Updating Property: " + currentPath + "," + newProperty + "=" + newValue);
+        }
+        response.sendRedirect("/home?zkPath=" + displayPath);
+    }
+
+    /**
+     * 搜索
+     *
+     * @param request
+     * @param response
+     * @param globalProps
+     * @param dao
+     * @param zkServerLst
+     * @param templateParam
+     * @param searchStr
+     * @param authRole
+     * @throws KeeperException
+     * @throws InterruptedException
+     * @throws IOException
+     * @throws TemplateException
+     */
+    private void search(HttpServletRequest request, HttpServletResponse response, Properties globalProps, Dao dao, String[] zkServerLst, Map<String, Object> templateParam, String searchStr, String authRole) throws KeeperException, InterruptedException, IOException, TemplateException {
+        ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+        Set<LeafBean> searchResult = ZooKeeperUtil.INSTANCE.searchTree(searchStr, zookeeper, authRole);
+        templateParam.put("searchResult", searchResult);
+        ServletUtil.INSTANCE.renderHtml(request, response, templateParam, "search.ftl.html");
+    }
+
+    /**
+     * 删除
+     *
+     * @param request
+     * @param response
+     * @param globalProps
+     * @param dao
+     * @param zkServerLst
+     * @param nodeChkGroup
+     * @param propChkGroup
+     * @param authRole
+     * @param displayPath
+     * @throws KeeperException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void delete(HttpServletRequest request, HttpServletResponse response, Properties globalProps, Dao dao, String[] zkServerLst, String[] nodeChkGroup, String[] propChkGroup, String authRole, String displayPath) throws KeeperException, InterruptedException, IOException {
+        if (authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
+
+            String user = (String) request.getSession().getAttribute("authName");
+
+            if (propChkGroup != null) {
+                for (String prop : propChkGroup) {
+                    List delPropLst = Arrays.asList(prop);
+                    ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+                    ZooKeeperUtil.INSTANCE.deleteLeaves(delPropLst, zookeeper);
+                    request.getSession().setAttribute("flashMsg", "Delete Completed!");
+
+                    dao.insertHistory(user, request.getRemoteAddr(), "Deleting Property: " + delPropLst.toString());
+                }
+            }
+            if (nodeChkGroup != null) {
+                for (String node : nodeChkGroup) {
+                    List delNodeLst = Arrays.asList(node);
+                    ZooKeeper zookeeper = ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps);
+                    ZooKeeperUtil.INSTANCE.deleteFolders(delNodeLst, zookeeper);
+                    request.getSession().setAttribute("flashMsg", "Delete Completed!");
+
+                    dao.insertHistory(user, request.getRemoteAddr(), "Deleting Nodes: " + delNodeLst.toString());
+                }
+            }
+
+        }
+        response.sendRedirect("/home?zkPath=" + displayPath);
     }
 }
